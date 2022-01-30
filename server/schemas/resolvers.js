8913f915a7c5e1830f4e19651475d40a70986ef8
Julius Markauskas <juliusm9791@gmail.com>
2022-01-30 18:47:44 -0500

    update on schema

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,14 +5,14 @@ const { signToken } = require('../utils/auth');
 const resolvers = {
   Query: {
     users: async () => {
-      return User.find({}).populate('savedBooks');
+      return await User.find({}).populate('savedBooks');
     },
     user: async (parent, { username }) => {
-      return User.findOne({ username }).populate('savedBooks');
+      return await User.findOne({ username }).populate('savedBooks');
     },
     me: async (parent, args, context) => {
       if (context.user) {
-        return User.findOne({ _id: context.user._id }).populate('savedBooks');
+        return await User.findOne({ _id: context.user._id }).populate('savedBooks');
       }
       throw new AuthenticationError('You need to be logged in!');
     },
@@ -43,33 +43,28 @@ const resolvers = {
       return { token, user };
     },
 
-    deleteBook: async (parent, { bookId }) => {
-      // if (context.user) {
+    deleteBook: async (parent, { bookId }, context) => {
+      if (context.user) {
 
-      const context = {
-        _id: "61f61147dbfc827e08210774"
-      }
-      console.log('0000', context._id , bookId);
-      return User.findOneAndUpdate(
+      return await User.findOneAndUpdate(
         { _id: context.user._id },
         { $pull: { savedBooks: { bookId: bookId } } },
         { new: true }
       );
-      // }
-      // throw new AuthenticationError('You need to be logged in!');
+      }
+      throw new AuthenticationError('You need to be logged in!');
     },
 
     saveBook: async (parent, args, context) => {
-      console.log(args, "1111");
-      console.log('0000', context._id);
-      // if (context.user) {
-      return User.findOneAndUpdate(
+      console.log(args);
+      if (context.user) {
+      return await User.findOneAndUpdate(
         { _id: context._id },
-        { $addToSet: { savedBooks: { ...args } } },
+        { $addToSet: { savedBooks: args } },
         { new: true }
       );
-      // }
-      // throw new AuthenticationError('You need to be logged in!');
+      }
+      throw new AuthenticationError('You need to be logged in!');
     },
 
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,10 +3,10 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
   type User {
     _id: ID
-    username: String
-    email: String
-    password: String
-    bookCount: String
+    username: String!
+    email: String!
+    password: String!
+    bookCount: Int
     savedBooks: [Book]
    }
 
